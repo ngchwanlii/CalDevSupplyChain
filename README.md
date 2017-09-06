01c270a83df9af616248651f1d43cb2e6b01f794
Calvin Liang <cliangdev@gmail.com>
2017-09-06 16:02:42 -0700

    project skeleton

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# CalDevSupplyChain
-Garment Supply Chain Service
+# CalDev Supply Chain Service
+
+## Installation
+- To launch the app, run `gradle clean bootRun`
+- Go to [http://localhost:8080/health] to verify that server is running.
---
+++ src/main/java/com/caldevsupplychain/CalDevSupplyChainApplication.java
@@ -0,0 +1,12 @@
+package com.caldevsupplychain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalDevSupplyChainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalDevSupplyChainApplication.class, args);
+	}
+}
